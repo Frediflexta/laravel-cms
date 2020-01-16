568657d38465476445efcdf571265823fbac21ee
frediflexta <fredrick.adegoke@gmail.com>
2020-01-16 16:57:05 +0100

    creat a crud for tags build out the views make the errors in creating resources reuseable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,6 +85,9 @@
                     <div class="col-md-4">
                         <ul class="list-group">
                             <li class="list-group-item">
+                                <a href={{ route('tags.index') }}>Tag</a>
+                            </li>
+                            <li class="list-group-item">
                                 <a href={{ route('posts.index')}}>Posts</a>
                             </li>
                             <li class="list-group-item">
